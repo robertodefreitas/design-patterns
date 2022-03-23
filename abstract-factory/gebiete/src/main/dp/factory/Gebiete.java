0736d58812e83a5a78218ea8e1855659b79c1689
Roberto <robertodefreitas@users.noreply.github.com>
2022-03-23 10:29:17 +0100

    update files

--- abstract-factory/gebiete/src/main/dp/factory/Gebiete.java
+++ abstract-factory/gebiete/src/main/dp/factory/Gebiete.java
@@ -4,23 +4,22 @@ import dp.factory.family.PolargebietFabrik;
 import dp.factory.interfaces.AbstrakteGebietfabrik;
 
 public class Gebiete {
-
-    /**
-     * Von einer PolargebietFabrik alle Schritte ausf�hren lassen
-     */
-    public Gebiete() {
-        AbstrakteGebietfabrik fabrik = new PolargebietFabrik();
-        var pflanze = fabrik.createPflanze();
-        var tier = fabrik.createTier();
-        var untergrund = fabrik.createUntergrund();
-    }
-
-    /**
-     * Ein neues Gebiet beginnen
-     *
-     * @param args werden ignoriert
-     */
-    public static void main(String[] args) {
-        Gebiete gebiet = new Gebiete();
-    }
+//    /**
+//     * Von einer PolargebietFabrik alle Schritte ausf�hren lassen
+//     */
+//    public Gebiete() {
+//        AbstrakteGebietfabrik fabrik = new PolargebietFabrik();
+//        var pflanze = fabrik.createPflanze();
+//        var tier = fabrik.createTier();
+//        var untergrund = fabrik.createUntergrund();
+//    }
+//
+//    /**
+//     * Ein neues Gebiet beginnen
+//     *
+//     * @param args werden ignoriert
+//     */
+//    public static void main(String[] args) {
+//        Gebiete gebiet = new Gebiete();
+//    }
 }
--- abstract-factory/gebiete/src/test/GebieteTest.java
+++ abstract-factory/gebiete/src/test/GebieteTest.java
@@ -1,31 +1,69 @@
 
-import dp.factory.Gebiete;
 import dp.factory.family.PolargebietFabrik;
+import dp.factory.family.RegenwaldFabrik;
 import dp.factory.interfaces.AbstrakteGebietfabrik;
 import dp.factory.interfaces.Pflanze;
 import dp.factory.interfaces.Tier;
 import dp.factory.interfaces.Untergrund;
+import dp.factory.product.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GebieteTest {
-    Gebiete gebiet = new Gebiete();
 
     public AbstrakteGebietfabrik createPolar() {
         AbstrakteGebietfabrik fabrik = new PolargebietFabrik();
         return fabrik;
     }
 
+    public AbstrakteGebietfabrik createRegenwald() {
+        AbstrakteGebietfabrik fabrik = new RegenwaldFabrik();
+        return fabrik;
+    }
+
+
     /* TESTS */
 
     @Test
     public void polarTest() throws Exception {
         AbstrakteGebietfabrik fabrik = createPolar();
-        Pflanze pflanze = fabrik.createPflanze();
-        Tier tier = fabrik.createTier();
-        Untergrund untergrund = fabrik.createUntergrund();
 
-        System.out.println("RESULT: " + pflanze.toString());
-        //Assertions.assertEquals(1120, ga.summeAlleMuenzenCentsKasse());
+        Pflanze pflanzeActual = fabrik.createPflanze();
+        Tier tierActual = fabrik.createTier();
+        Untergrund untergrundActual = fabrik.createUntergrund();
+
+        Pflanze pflanzeExpected = new Flechte();
+        Tier tierExpected = new Eisbaer();
+        Untergrund untergrundExpected = new Schnee();
+
+        //System.out.println("RESULT: " + pflanzeActual.getClass());
+
+        Assertions.assertFalse(pflanzeExpected.equals(pflanzeActual));
+
+        Assertions.assertEquals(pflanzeExpected.getClass(), pflanzeActual.getClass());
+        Assertions.assertEquals(tierExpected.getClass(), tierActual.getClass());
+        Assertions.assertEquals(untergrundExpected.getClass(), untergrundActual.getClass());
+    }
+
+    @Test
+    public void regenwaldTest() throws Exception {
+        AbstrakteGebietfabrik fabrik = createRegenwald();
+
+        Pflanze pflanzeActual = fabrik.createPflanze();
+        Tier tierActual = fabrik.createTier();
+        Untergrund untergrundActual = fabrik.createUntergrund();
+
+        Pflanze pflanzeExpected = new Baum();
+        Tier tierExpected = new Girafe();
+        Untergrund untergrundExpected = new Gras();
+
+        //System.out.println("RESULT: " + pflanzeActual.getClass());
+
+        Assertions.assertFalse(pflanzeExpected.equals(pflanzeActual));
+
+        Assertions.assertEquals(pflanzeExpected.getClass(), pflanzeActual.getClass());
+        Assertions.assertEquals(tierExpected.getClass(), tierActual.getClass());
+        Assertions.assertEquals(untergrundExpected.getClass(), untergrundActual.getClass());
     }
 
 }
